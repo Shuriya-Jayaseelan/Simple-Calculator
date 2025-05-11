67d8bf3a2f390ae3ae3ac3b5625f7b58759a141e
Shuriya-Jayaseelan <shuriya2805@gmail.com>
2025-05-11 14:58:40 +0800

    Remodified the code to handle division by zero and print formatting

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -5,20 +5,26 @@ number_2 = float(input("Enter Second Number: "))
 
 print("\nChoose an operation\n" + "1. Addition\n" + "2. Subtraction\n" + "3. Multiplication\n" + "4. Division\n")
 choice = int(input("Option: "))
+result = 0
 
-if  (choice == 1) :
-    print ("\nYour Total is", number_1 + number_2)
+if choice == 1:
+    result = number_1 + number_2
 
-elif (choice == 2) :
-    print ("\nYour Total is", number_1 - number_2)
+elif choice == 2:
+    result = number_1 - number_2
 
-elif (choice == 3) :
-    print ("\nYour Total is", number_1 * number_2)
+elif choice == 3:
+    result = number_1 * number_2
 
-elif (choice == 4) :
-    print ("\nYour Total is", number_1 / number_2)
+elif choice == 4:
+    if number_2 == 0:
+        print("Cannot Divide By Zero!")
+        exit()
+    result = number_1 / number_2
 
 else :
-    print("\nYou have entered an invalid operation!")
+    print("\nYou have chosen an invalid option! Please try again later!")
+
+print("\nThe Result is {r:1.3}".format(r = result))
 
 print("\nThank you for Using This Program!!")
